d39a23fe0fcb6b1bcf6851bab4d1ea988c5546c7
Audrey <audreywang2006@gmail.com>
2020-03-29 18:01:59 -0700

    data edit(qqq)

--- yf1.py
+++ yf1.py
@@ -3,4 +3,8 @@ import yfinance as yf
 msft = yf.Ticker("MSFT")
 print(msft)
 print(msft.info)
-print(msft.history(period="max"))
+print(msft.history(period="ytd"))
+
+data_df = yf.download("QQQ", start="2020-01-01", end="2020-03-28")
+data_df.to_csv("qqq.csv")
+
