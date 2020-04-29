5d6524222eb539f256f703e8276fb8cd1b4cdd12
audreyolaf <62197228+audreyolaf@users.noreply.github.com>
2020-04-28 18:57:00 -0700

    Delete lab12.py

--- lab12.py
+++
@@ -1,8 +0,0 @@
-#magic 8 ball
-import random
-ans = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", " Yes, definitely.", "You may rely on it."]
-while True:
-    user_input = input("Ask a question: ")
-    sel = random.choice(ans)
-    print("Thinking...")
-    print(sel + "\n")
